8ff98ed317ce1c5871f8dd3ac1401147e8b280b5
mahdi akhbar <mahdi.akhbar32@gmail.com>
2021-03-17 18:24:53 +0330

    SlotMachine

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,61 @@
-
 #include <iostream>
 #include <stdlib.h>
+#include <cstdlib>
+#include <time.h>
 
-#include "Foo.h"
+int Random(int low, int high);
 int main()
 {
-  Foo d = Foo();
-  std::cout<< d.doSomething()<<std::endl;
-
+  srand(time(0));
+  int rand1{}, rand2{}, rand3{};
+  int choice;
+  int chips{1000};
+  int bet{};
+  do{
+    std::cout << "Player's chips: $" << chips << '\n';
+    std::cout << "1) Play slot. 2) Exit. ";
+    std::cin >> choice;
+    switch (choice){
+      case 1:{
+        while (true){
+          std::cout << "Enter your bet: ";
+          std::cin >> bet;
+          if (bet > chips || bet <= 0)
+          std::cout << "You did not enter a valid bet." << '\n';
+          else
+          break;
+        }//while (bet > chips || bet <= 0);
+        rand1 = Random(2, 7);
+        rand2 = Random(2, 7);
+        rand3 = Random(2, 7);
+        bool seven = rand1 == 7 && rand2 == 7 && rand3 == 7;
+        bool three = !seven && (rand1 == rand2 && rand1 == rand3 && rand2 == rand3);
+        bool two   = (rand1 == rand2) || (rand1 == rand3) || (rand2 == rand3);
+        std::cout << rand1 << " " << rand2 << " " << rand3 << '\n';
+        if (seven) {
+          chips += bet * 10;
+          std::cout << "You win!" << '\n';
+        }
+        else if (three) {
+          chips += bet * 5;
+          std::cout << "You win!" << '\n';
+        }
+        else if (two){
+          chips += bet * 3;
+          std::cout << "You win" << '\n';
+        }
+        else
+        chips -= bet;
+      }break;
+      case 2:
+        std::cout << "Exiting..." << '\n';
+        break;
+    }
+  }while (choice != 2 || chips <= 0);
   return 0;
+}
+int Random(int low, int high){
+  srand(time(0));
+  int random = low + rand() % ((high + 1) - low);
+  return random;
 }
